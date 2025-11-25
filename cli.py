39685a714d3375cc514404bdf19154fb5e70d710
ukaznil <ukaznil@gmail.com>
2025-11-25 12:10:11 +0900

    format cli.py

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
 import argparse
 
-from midnight.midnight_app import MidNightApp
+from base_app import BaseApp
+from midnight.midnight_app import MidnightApp
 from project import Project
 
 
@@ -10,63 +11,118 @@ def build_parser() -> argparse.ArgumentParser:
         )
     parser.add_argument(
         '-p', '--project',
-        type=str, required=True,
+        type=str,
+        required=True,
         choices=['midnight', 'defensio'],
+        help='Target project to use.',
         )
     subparsers = parser.add_subparsers(
         dest='command',
         required=True,
-        help='Sub commands',
+        help='Top level command to run.',
         )
 
     # -------------------------
     # wallet sub-command
     # -------------------------
     wallet_parser = subparsers.add_parser(
-        'register',
+        'wallet',
+        description='Wallet related commands.',
+        help='Manage wallet addresses.',
         )
-    wallet_parser.add_argument(
+    wallet_subparsers = wallet_parser.add_subparsers(
+        dest='wallet_command',
+        required=True,
+        help='Wallet sub commands.',
+        )
+
+    # wallet register -a ...
+    wallet_register_parser = wallet_subparsers.add_parser(
+        'register',
+        description='Register a new wallet address to be used for mining rewards.',
+        help='Register a wallet address.',
+        )
+    wallet_register_parser.add_argument(
         '-a', '--address',
         type=str,
         required=True,
+        help='Wallet address to register.',
         )
-    wallet_parser.set_defaults(handler='wallet')
+    wallet_register_parser.set_defaults(handler='register_wallet')
+
+    # wallet list
+    wallet_list_parser = wallet_subparsers.add_parser(
+        'list',
+        description='List all wallet addresses registered for this project.',
+        help='Show registered wallet addresses.',
+        )
+    wallet_list_parser.set_defaults(handler='list_wallets')
 
     # -------------------------
     # mine sub-command
     # -------------------------
     mine_parser = subparsers.add_parser(
         'mine',
+        description='Start the miner for the selected project.',
+        help='Start mining.',
         )
     mine_parser.add_argument(
         '-t', '--num_threads',
         type=int,
+        help='Number of miner threads to spawn.',
         )
     mine_parser.set_defaults(handler='mine')
 
     return parser
 
 
+# -------------------------
+# handlers
+# -------------------------
+def handle_register_wallet(app: BaseApp, args: argparse.Namespace) -> None:
+    app.handle_register(address=args.address)
+
+
+def handle_list_wallet(app: BaseApp, args: argparse.Namespace) -> None:
+    app.handle_list_wallets()
+
+
+def handle_mine(app: BaseApp, args: argparse.Namespace) -> None:
+    app.handle_mine(num_threads=args.num_threads)
+
+
+# -------------------------
+# main
+# -------------------------
 def main(argv=None) -> int:
     parser = build_parser()
     args = parser.parse_args(argv)
 
     if args.project == 'midnight':
-        project = Project.MidNight
+        app = MidnightApp(project=Project.MidNightt)
     elif args.project == 'defensio':
-        project = Project.Defensio
+        app = MidnightApp(project=Project.Defensio)
     else:
         raise NotImplementedError(args.project)
     # endif
 
-    app = MidNightApp(project=project)
-    if args.command == 'register':
-        app.handle_register(address=args.address)
-    elif args.command == 'mine':
-        app.handle_mine(num_threads=args.num_threads)
-    # endif
+    handlers = {
+        'register_wallet': handle_register_wallet,
+        'list_wallets': handle_list_wallet,
+        'mine': handle_mine,
+        }
 
-    return 0
+    handler_key = getattr(args, 'handler', None)
+    handler = handlers.get(handler_key)
+    if handler is None:
+        parser.print_help()
+
+        return 1
+    else:
+        handler(app, args)
+
+        return 0
+    # endif
 
 
 if __name__ == "__main__":
